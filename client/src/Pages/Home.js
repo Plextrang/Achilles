5d3265d84a596603a824e14116e9ae832dcbd3f1
Sneha Jacob <jacobsneha949@gmail.com>
2024-03-18 02:17:32 -0500

    homepage

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import walking from '../images/converse.jpg'
 import "./Home.css"
+
 
 export default function Home() {
   return (
     <div className="home-container">
-        <p>Hello World!</p>
+      <img src ={walking}/>
+        <h1>Achilles</h1>
+        <p>Step with Style</p>
     </div> 
   )
 }
