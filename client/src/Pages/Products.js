b7b4d5906fbda9bf135b5a235beb895f0a888ebc
Sneha Jacob <jacobsneha949@gmail.com>
2024-03-20 01:08:11 -0500

    filter bar

--- client/src/Pages/Products.js
+++ client/src/Pages/Products.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import "./Products.css"
+import white_converse from "../images/white_converse.jpg"
 
 export default function Products(){
     return(
         <div className = 'product-container'>
-            <p>TODO: ADD STUFF HERE</p>
+            <header>
+                <h1>shop now.</h1>
+                <nav className='filters'>
+                <a href='#' className = "active">All</a>
+                <a href = '#'>Womens</a>
+                <a href = '#'>Mens</a>
+                <a href = '#'>Kids</a>
+                </nav>
+             </header>  
         </div>
+        
     )
 }
