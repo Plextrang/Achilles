1e493f49759733357319c94d340885436b8f4159
christ0f3r <142352888+christ0f3r@users.noreply.github.com>
2024-03-20 18:32:17 -0500

    Merge branch 'main' of https://github.com/snehajac1/COSC-3380

--- client/src/Pages/Products.js
+++ client/src/Pages/Products.js
@@ -2,6 +2,8 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import "./Products.css"
 import white_converse from "../images/white_converse.jpg"
+import { FaStar } from "react-icons/fa";
+import { FaShoppingBag } from "react-icons/fa";
 
 export default function Products(){
     return(
@@ -15,7 +17,29 @@ export default function Products(){
                 <a href = '#'>Kids</a>
                 </nav>
              </header>  
+
+            <div className="card-container">
+            <section className="card">
+                <img src={white_converse} alt='White Converse' 
+                />
+                <div className="card-details">
+                    <h3 classname="card-title">Converse</h3>
+                    <section className="card-reviews">
+                        <FaStar></FaStar>
+                        <span className="total-reviews">4</span>
+                    </section>
+                    <section className="card-price">
+                        <div className="price">$80</div>
+                    </section>
+                    <div className="bag">
+                        <FaShoppingBag></FaShoppingBag>
+                    </div>
+                </div>
+            </section>
         </div>
+        
+        </div>
+        
         
     )
 }
