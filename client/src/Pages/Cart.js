d647a30a94556372b1d91fd451b36dfeedf768df
RaianM <momin.raian@gmail.com>
2024-03-21 15:37:50 -0500

    Adding Basic Shopping Cart Page 2

--- client/src/Pages/Cart.js
+++ client/src/Pages/Cart.js
@@ -26,7 +26,6 @@ export default function Cart() {
                     </div>
                     <button className="cart-item-remove">Remove</button>
                 </div>
-                
             </div>
             <div className="cart-summary">
                 <h2>Cart Summary</h2>
@@ -35,6 +34,12 @@ export default function Cart() {
                     <span>-add logic to get total-</span>
                 </div>
                 <button className="checkout-button">Checkout</button>
+            </div>
+            <div className="payment-section">
+                <h2>Payment Method</h2>
+                <p>Credit Card</p>
+                <h2>Shipping Address</h2>
+                <p>123 Shipping Street, City, Country</p>
             </div>
         </div>
     );
