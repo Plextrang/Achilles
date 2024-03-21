79af10517ac8a7338f4771c27301e6cfd722b96b
SeanEppling <epplingsean@gmail.com>
2024-03-21 11:36:29 -0500

    newUser makes entry, need to change query

--- server/handlers/userHandler.js
+++ server/handlers/userHandler.js
@@ -3,28 +3,31 @@ const mysql = require('mysql');
 const cors = require('cors');
 const querystring = require('querystring');
 
-function newUser(req){
-    let body = '';
+function newUser(req, db, res){
+    body = ""
     req.on('data', chunk => {
         body += chunk.toString(); // convert Buffer to string
     });
-
+    
     req.on('end', () => {
-        const userData = querystring.parse(body);
-        const { email, password, firstName, lastName, phoneNumber, dateOfBirth, address } = userData;
+        console.log('Received data:', body);
+        const userData = JSON.parse(body);
+        console.log('Parsed user data:', userData);
+        const { email, password, firstName, lastName, phoneNumber, dateOfBirth } = userData;
 
-        // Insert email and password into the 'credentials' table
-        const insertCredentialsSql = `INSERT INTO credentials (email, password) VALUES ('${email}', '${password}')`;
-        db.query(insertCredentialsSql, (err, credentialsResult) => {
+        const insertCredentialsSql = `INSERT INTO LOGIN (email, password) VALUES (?, ?)`;
+        db.query(insertCredentialsSql, [email, password], (err, credentialsResult) => {
             if (err) {
+                console.error(err);
                 res.end(JSON.stringify({ error: 'Internal Server Error' }));
                 return;
             }
 
-            // Insert the rest of the user details into the 'users' table
-            const insertUserSql = `INSERT INTO users (email, first_name, last_name, phone_number, date_of_birth, address) VALUES (${email}, '${firstName}', '${lastName}', '${phoneNumber}', '${dateOfBirth}', '${address}')`;
-            db.query(insertUserSql, (err, userResult) => {
+            // Ensure phoneNumber is treated as a string
+            const insertUserSql = `INSERT INTO USER (email, first_name, last_name, phone_number, date_of_birth) VALUES (?, ?, ?, ?, ?)`;
+            db.query(insertUserSql, [email, firstName, lastName, phoneNumber.toString(), dateOfBirth], (err, userResult) => {
                 if (err) {
+                    console.error(err);
                     res.end(JSON.stringify({ error: 'Internal Server Error' }));
                     return;
                 }
