4d79b59350e0b4fcb4bc26f9a00be40f46f1263c
RaianM <momin.raian@gmail.com>
2024-03-21 12:36:37 -0500

    Updating Products View Page

--- client/src/Pages/Products.js
+++ client/src/Pages/Products.js
@@ -1,45 +1,123 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import "./Products.css"
-import white_converse from "../images/white_converse.jpg"
-import { FaStar } from "react-icons/fa";
-import { FaShoppingBag } from "react-icons/fa";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Products.css';
+import white_converse from '../images/white_converse.jpg';
+import { FaStar } from 'react-icons/fa';
+import { FaShoppingBag } from 'react-icons/fa';
 
-export default function Products(){
-    return(
-        <div className = 'product-container'>
-            <header className = "title">
-                <h1 className = "shop-now-container">shop now.</h1>
-                <nav className='filters'>
-                <a href='#' className = "active">All</a>
-                <a href = '#'>Womens</a>
-                <a href = '#'>Mens</a>
-                <a href = '#'>Kids</a>
-                </nav>
-             </header>  
+export default function Products() {
+  return (
+    <div className="product-container">
+      <header className="title">
+        <h1 className="shop-now-container">Our Products</h1>
+        <div className="search-bar">
+          <input type="text" placeholder="Search..." />
+          <button>Search</button>
+        </div>
+        <nav className="filters">
+          <a href="#" className="active">
+            All
+          </a>
+          <a href="#">Womens</a>
+          <a href="#">Mens</a>
+          <a href="#">Kids</a>
+        </nav>
+      </header>
 
-            <div className="card-container">
-            <section className="card">
-                <img src={white_converse} alt='White Converse' 
-                />
-                <div className="card-details">
-                    <h3 classname="card-title">Converse</h3>
-                    <section className="card-reviews">
-                        <FaStar></FaStar>
-                        <span className="total-reviews">4</span>
-                    </section>
-                    <section className="card-price">
-                        <div className="price">$80</div>
-                    </section>
-                    <div className="bag">
-                        <FaShoppingBag></FaShoppingBag>
-                    </div>
-                </div>
+      <div className="card-container">
+        <div className="card">
+          <img src={white_converse} alt="White Converse" />
+          <div className="card-details">
+            <h3 className="card-title">Women's Converse</h3>
+            <section className="card-reviews">
+              <FaStar />
+              <span className="total-reviews">4 Reviews</span>
             </section>
+            <div className="bag">
+              <FaShoppingBag /> 
+              <div className="price">$80</div>
+            </div>
+          </div>
+        </div>
+
+        <div className="card">
+          <img src={white_converse} alt="White Converse" />
+          <div className="card-details">
+            <h3 className="card-title">Men's Converse</h3>
+            <section className="card-reviews">
+              <FaStar />
+              <span className="total-reviews">4 Reviews</span>
+            </section>
+            <div className="bag">
+              <FaShoppingBag /> 
+              <div className="price">$80</div>
+            </div>
+          </div>
+        </div>
+
+        <div className="card">
+          <img src={white_converse} alt="White Converse" />
+          <div className="card-details">
+            <h3 className="card-title">Kid's Converse</h3>
+            <section className="card-reviews">
+              <FaStar />
+              <span className="total-reviews">4 Reviews</span>
+            </section>
+            <div className="bag">
+              <FaShoppingBag /> 
+              <div className="price">$80</div>
+            </div>
+          </div>
+        </div>
+
+        <div className="card">
+          <img src={white_converse} alt="White Converse" />
+          <div className="card-details">
+            <h3 className="card-title">100% Real Converse</h3>
+            <section className="card-reviews">
+              <FaStar />
+              <span className="total-reviews">4 Reviews</span>
+            </section>
+            <div className="bag">
+              <FaShoppingBag /> 
+              <div className="price">$80</div>
+            </div>
+          </div>
+        </div>
+
+        <div className="card">
+          <img src={white_converse} alt="White Converse" />
+          <div className="card-details">
+            <h3 className="card-title">Not Fake Converse</h3>
+            <section className="card-reviews">
+              <FaStar />
+              <span className="total-reviews">4 Reviews</span>
+            </section>
+            <div className="bag">
+              <FaShoppingBag /> 
+              <div className="price">$80</div>
+            </div>
+          </div>
+        </div>
+
+        <div className="card">
+          <img src={white_converse} alt="White Converse" />
+          <div className="card-details">
+            <h3 className="card-title">It's Surprisingly Converse</h3>
+            <section className="card-reviews">
+              <FaStar />
+              <span className="total-reviews">4 Reviews</span>
+            </section>
+            <div className="bag">
+              <FaShoppingBag /> 
+              <div className="price">$80</div>
+            </div>
+          </div>
         </div>
         
-        </div>
-        
-        
-    )
+
+        {/* Add more card containers for other products */}
+      </div>
+    </div>
+  );
 }
