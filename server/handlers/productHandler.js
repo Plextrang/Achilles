c4338b186d11ec05c5ef4cfea9fa5b4673bfddb3
Christofer Laygo <142352888+christ0f3r@users.noreply.github.com>
2024-03-23 01:07:01 -0500

    Attempted to retrieve products from database
    Updated productHandler.js and server.js to GET data from the database to display on Products.js page, but not successful. I give up :anguished:

--- server/handlers/productHandler.js
+++ server/handlers/productHandler.js
@@ -3,6 +3,28 @@ const mysql = require('mysql');
 const cors = require('cors');
 const querystring = require('querystring');
 
+// Function to handle GET request for fetching product data
+// req = request
+// res = response
+function getProducts(req, res, db) {
+    // query string
+    const sql = 'SELECT * FROM products';
+  
+    // executes the SQL query using the 'db' connection
+    // sql -> sql that specifies the operation you want to perform on the database
+    // err -> any errors that the database driver gets will be passed to 'err'
+    // results -> results of the query will be held in results
+    db.query(sql, (err, results) => {
+      if (err) {
+        console.error('Error fetching products:', err);
+        // Logs error to the console (500)
+        res.status(500).json({ error: 'Internal Server Error' });
+      } else {
+        // 200 means data is sucessfully being sent back
+        // json(results) serializes the retrieved data (results) into JSON format before sending it back to the client
+        res.status(200).json(results);
+      }
+    });
+  }
 
-
-module.exports = {}
+module.exports = {getProducts}
