e07b91149229d6952489c73a55872d84bebe0327
SeanEppling <epplingsean@gmail.com>
2024-03-23 21:52:53 -0500

    mhm

--- client/src/Pages/Products.js
+++ client/src/Pages/Products.js
@@ -11,8 +11,7 @@ export default function Products() {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    //fetch('http://localhost:12358/getProducts')
-    fetch('https://cosc-3380-backend.vercel.app/getProducts')
+    fetch('http://localhost:12358/getProducts')
       .then(response => {
         if (!response.ok) {
           throw new Error('Network response was not ok');
