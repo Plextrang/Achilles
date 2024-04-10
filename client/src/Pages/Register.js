98dfc4c187c62e66eacdd75b014775142628cfc0
SeanEppling <epplingsean@gmail.com>
2024-04-10 17:14:04 -0500

    yippeeeeeeeeeeeeeeeeee

--- client/src/Pages/Register.js
+++ client/src/Pages/Register.js
@@ -55,7 +55,7 @@ export default function Register() {
 		};
 		
 		try {
-			console.log(userData)
+			console.log(JSON.stringify(userData))
 			const response = await fetch('https://cosc-3380-6au9.vercel.app/api/handlers/users/newUser', {
 				// mode: 'no-cors',
 				method: 'POST',
