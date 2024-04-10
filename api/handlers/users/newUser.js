4657d157becb8064ea7cc3da8fd87a272a43d0b0
SeanEppling <epplingsean@gmail.com>
2024-04-10 14:51:53 -0500

    yippeeee

--- api/handlers/users/newUser.js
+++ api/handlers/users/newUser.js
@@ -19,7 +19,22 @@ const querystring = require('querystring');
 //   });
 
 module.exports = async (req, res) => {
-    setCorsHeaders(req, res);
+    const allowCors = fn => async (req, res) => {
+        res.setHeader('Access-Control-Allow-Credentials', true)
+        res.setHeader('Access-Control-Allow-Origin', '*')
+        // another common pattern
+        // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+        res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+        res.setHeader(
+          'Access-Control-Allow-Headers',
+          'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+        )
+        if (req.method === 'OPTIONS') {
+          res.status(200).end()
+          return
+        }
+        return await fn(req, res)
+      }
     const db = mysql.createConnection({
         host: "cosc3380.c5iqeciq8qjg.us-east-2.rds.amazonaws.com",
         user: "admin",
@@ -70,3 +85,4 @@ module.exports = async (req, res) => {
         });
     });
 }
+module.exports = allowCors(handler)
