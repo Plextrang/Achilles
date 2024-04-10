ffa3d1c63ff54ba1addd9ed0b4b289341960f442
SeanEppling <epplingsean@gmail.com>
2024-04-10 16:08:55 -0500

    yippeeeeeeeeeeeee

--- api/handlers/users/newUser.js
+++ api/handlers/users/newUser.js
@@ -34,7 +34,6 @@ module.exports = async (req, res) => {
         console.log('Connected to database');
       });
     
-    body = ""
     const newbody = new Promise((resolve, reject) => {
         let body = '';
         req.on('data', chunk => {
@@ -54,31 +53,31 @@ module.exports = async (req, res) => {
           });
         });
 
-        console.log('Received data:', body);
-        const userData = await newbody;
-        console.log('Parsed user data:', userData);
-        const { email, password, first_name, middle_initial, last_name, phone_number, date_of_birth, address, apt_num, city, state, zip_code } = userData;
-        const type = 'Customer'
+    console.log('Received data:', body);
+    const userData = await newbody;
+    console.log('Parsed user data:', userData);
+    const { email, password, first_name, middle_initial, last_name, phone_number, date_of_birth, address, apt_num, city, state, zip_code } = userData;
+    const type = 'Customer'
 
-        const insertCredentialsSql = `INSERT INTO LOGIN (email, password) VALUES (?, ?)`;
-        db.query(insertCredentialsSql, [email, password], (err, loginResult) => {
+    const insertCredentialsSql = `INSERT INTO LOGIN (email, password) VALUES (?, ?)`;
+    db.query(insertCredentialsSql, [email, password], (err, loginResult) => {
+        if (err) {
+            console.error(err);
+            res.writeHead(404, { 'Content-Type' : 'application/json' });
+            res.end(JSON.stringify({ error: 'Login Internal Server Error' }));
+            return;
+        }
+
+        const insertUserSql = `INSERT INTO USER (email, first_name, middle_initial, last_name, phone_number, date_of_birth, address, apt_num, city, state, zip_code, user_type) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+        db.query(insertUserSql, [email, first_name, middle_initial, last_name, phone_number, date_of_birth, address, apt_num, city, state, zip_code, type], (err, userResult) => {
             if (err) {
-                console.error(err);
-                res.writeHead(404, { 'Content-Type' : 'application/json' });
-                res.end(JSON.stringify({ error: 'Login Internal Server Error' }));
-                return;
-            }
-
-            const insertUserSql = `INSERT INTO USER (email, first_name, middle_initial, last_name, phone_number, date_of_birth, address, apt_num, city, state, zip_code, user_type) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
-            db.query(insertUserSql, [email, first_name, middle_initial, last_name, phone_number, date_of_birth, address, apt_num, city, state, zip_code, type], (err, userResult) => {
-                if (err) {
                     console.error(err);
                     res.writeHead(404, { 'Content-Type' : 'application/json' });
                     res.end(JSON.stringify({ error: 'User Internal Server Error' }));
                     return;
                 }
                 console.log("inserted yippee")
-                res.writeHead(404, { 'Content-Type' : 'application/json' });
+                res.writeHead(200, { 'Content-Type' : 'application/json' });
                 res.end(JSON.stringify({ message: 'User added successfully', redirectUrl: '/Login' }));
                 // res.status(200).json({
                 //     message: 'User added successfully', redirectUrl: '/Login'
