affaec7198953df9a4318f592acdd8fa8c25904f
SeanEppling <epplingsean@gmail.com>
2024-04-10 21:05:44 -0500

    yippeeeeeeeeeeeeeeeeeeeeeeeeeee

--- api/handlers/users/newUser.js
+++ api/handlers/users/newUser.js
@@ -5,21 +5,8 @@ const mysql = require('mysql');
 const cors = require('cors');
 const querystring = require('querystring');
 
-// const db = mysql.createConnection({
-//     host: "cosc3380.c5iqeciq8qjg.us-east-2.rds.amazonaws.com",
-//     user: "admin",
-//     password: "TtZDqS57PM8KxHaOLRcs",
-//     database: "cosc3380"
-// })
-// db.connect(err => {
-//     if (err) {
-//       console.error('Error connecting to database:', err);
-//       return;
-//     }
-//     console.log('Connected to database');
-//   });
-
 module.exports = async (req, res) => {
+    console.log(req);
     setCorsHeaders(req, res);
     const db = mysql.createConnection({
         host: "cosc3380.c5iqeciq8qjg.us-east-2.rds.amazonaws.com",
