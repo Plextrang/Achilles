5a36d46b49797de9a916901c55bfd6b2acd2cced
SeanEppling <epplingsean@gmail.com>
2024-04-11 09:50:08 -0500

    hiiiiiii

--- api/handlers/users/newUser.js
+++ api/handlers/users/newUser.js
@@ -7,49 +7,51 @@ const querystring = require('querystring');
 
 module.exports = async (req, res) => {
     // console.log("this is the req \n", req);
-    setCorsHeaders(req, res);
-    const db = mysql.createConnection({
-        host: "cosc3380.c5iqeciq8qjg.us-east-2.rds.amazonaws.com",
-        user: "admin",
-        password: "TtZDqS57PM8KxHaOLRcs",
-        database: "cosc3380"
-    })
-    db.connect(err => {
-        if (err) {
-          console.error('Error connecting to database:', err);
-          return;
-        }
-        console.log('Connected to database');
-      });
+    res.writeHead(200, { 'Content-Type' : 'application/json' });
+    res.end(JSON.stringify({ message: 'yayy' }))
+    // setCorsHeaders(req, res);
+    // const db = mysql.createConnection({
+    //     host: "cosc3380.c5iqeciq8qjg.us-east-2.rds.amazonaws.com",
+    //     user: "admin",
+    //     password: "TtZDqS57PM8KxHaOLRcs",
+    //     database: "cosc3380"
+    // })
+    // db.connect(err => {
+    //     if (err) {
+    //       console.error('Error connecting to database:', err);
+    //       return;
+    //     }
+    //     console.log('Connected to database');
+    //   });
     
-    const userData = await getRequestBody(req, res)
-    console.log("this is the user data \n", userData);
-    const { email, password, first_name, middle_initial, last_name, phone_number, date_of_birth, address, apt_num, city, state, zip_code } = userData;
-    const type = 'Customer'
+    // const userData = await getRequestBody(req, res)
+    // console.log("this is the user data \n", userData);
+    // const { email, password, first_name, middle_initial, last_name, phone_number, date_of_birth, address, apt_num, city, state, zip_code } = userData;
+    // const type = 'Customer'
 
-    const insertCredentialsSql = `INSERT INTO LOGIN (email, password) VALUES (?, ?)`;
-    db.query(insertCredentialsSql, [email, password], (err, loginResult) => {
-        if (err) {
-            console.error(err);
-            res.writeHead(404, { 'Content-Type' : 'application/json' });
-            res.end(JSON.stringify({ error: 'Login Internal Server Error' }));
-            return;
-        }
+    // const insertCredentialsSql = `INSERT INTO LOGIN (email, password) VALUES (?, ?)`;
+    // db.query(insertCredentialsSql, [email, password], (err, loginResult) => {
+    //     if (err) {
+    //         console.error(err);
+    //         res.writeHead(404, { 'Content-Type' : 'application/json' });
+    //         res.end(JSON.stringify({ error: 'Login Internal Server Error' }));
+    //         return;
+    //     }
 
-        const insertUserSql = `INSERT INTO USER (email, first_name, middle_initial, last_name, phone_number, date_of_birth, address, apt_num, city, state, zip_code, user_type) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
-        db.query(insertUserSql, [email, first_name, middle_initial, last_name, phone_number, date_of_birth, address, apt_num, city, state, zip_code, type], (err, userResult) => {
-            if (err) {
-                    console.error(err);
-                    res.writeHead(404, { 'Content-Type' : 'application/json' });
-                    res.end(JSON.stringify({ error: 'User Internal Server Error' }));
-                    return;
-                }
-                console.log("inserted yippee")
-                res.writeHead(200, { 'Content-Type' : 'application/json' });
-                res.end(JSON.stringify({ message: 'User added successfully', redirectUrl: '/Login' }));
-                // res.status(200).json({
-                //     message: 'User added successfully', redirectUrl: '/Login'
-                // })
-            });
-        });
+    //     const insertUserSql = `INSERT INTO USER (email, first_name, middle_initial, last_name, phone_number, date_of_birth, address, apt_num, city, state, zip_code, user_type) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+    //     db.query(insertUserSql, [email, first_name, middle_initial, last_name, phone_number, date_of_birth, address, apt_num, city, state, zip_code, type], (err, userResult) => {
+    //         if (err) {
+    //                 console.error(err);
+    //                 res.writeHead(404, { 'Content-Type' : 'application/json' });
+    //                 res.end(JSON.stringify({ error: 'User Internal Server Error' }));
+    //                 return;
+    //             }
+    //             console.log("inserted yippee")
+    //             res.writeHead(200, { 'Content-Type' : 'application/json' });
+    //             res.end(JSON.stringify({ message: 'User added successfully', redirectUrl: '/Login' }));
+    //             // res.status(200).json({
+    //             //     message: 'User added successfully', redirectUrl: '/Login'
+    //             // })
+    //         });
+    //     });
 }
