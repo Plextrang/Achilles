ce5397ad696fb182dba1d9bc12ccbc1141307c49
christ0f3r <142352888+christ0f3r@users.noreply.github.com>
2024-04-12 19:06:20 -0500

    Imported more shoes, looks weird though :(

--- client/src/Pages/Products.js
+++ client/src/Pages/Products.js
@@ -3,10 +3,19 @@ import { FaStar, FaShoppingBag } from 'react-icons/fa';
 import {Link} from 'react-router-dom';
 import white_converse from '../images/white_converse.jpg';
 import nike_air_force_1 from '../images/nike_air_force_1.jpg';
+import adidas_gazelle_blue_gold from '../images/adidas_gazella_blue_gold.jpg';
+import doc_martens_jorge from '../images/doc_martens_jorge.jpg';
+import hk_crocs_clogs from '../images/hk_crocs_clogs.jpg';
+import naruto_crocs_clog from '../images/naruto_crocs_clog.jpg';
 import "./Products.css"
 
 const variableMap = {
-  'white_converse': white_converse
+  'white_converse': white_converse,
+  'nike_air_force_1': nike_air_force_1,
+  'adidas_gazelle_blue_gold': adidas_gazelle_blue_gold,
+  'doc_martens_jorge': doc_martens_jorge,
+  'hk_crocs_clogs': hk_crocs_clogs,
+  'naruto_crocs_clog': naruto_crocs_clog
 };
 
 export default function Products() {
