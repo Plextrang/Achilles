a14356fd188da4b8e7d79d126aed8dfac17b513b
Sneha Jacob <jacobsneha949@gmail.com>
2024-04-14 22:32:05 -0500

    i'll continue this tmrw :((

--- client/src/Pages/CheckOut.js
+++ client/src/Pages/CheckOut.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import white_converse from '../images/white_converse.jpg'; // Import image
+import React, {useEffect, useState,} from 'react';
+import white_converse from '../images/white_converse.jpg'; 
+import nike_air_force_1 from '../images/nike_air_force_1.jpg';
+import adidas_gazelle_blue_gold from '../images/adidas_gazella_blue_gold.jpg';
+import doc_martens_jorge from '../images/doc_martens_jorge.jpg';
+import hk_crocs_clogs from '../images/hk_crocs_clogs.jpg';
+import naruto_crocs_clog from '../images/naruto_crocs_clog.jpg';
 import './CheckOut.css';
 
 // Pull all shoes from cart item where the user_id matches
 // display the shoes one at a time
 // add the totals of all shoes by adding the price*quantity for each product.
+const variableMap = {
+    'white_converse': white_converse,
+    'nike_air_force_1': nike_air_force_1,
+    'adidas_gazelle_blue_gold': adidas_gazelle_blue_gold,
+    'doc_martens_jorge': doc_martens_jorge,
+    'hk_crocs_clogs': hk_crocs_clogs,
+    'naruto_crocs_clog': naruto_crocs_clog
+  };
+
 
 export default function CheckOut() {
+  
     return (
         <div className="checkout-container">
             <h1 className="checkout-title">Checkout</h1>
