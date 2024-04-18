59466806fd0607f24fd9458c9f78b5ecba1e3a95
christ0f3r <142352888+christ0f3r@users.noreply.github.com>
2024-04-18 17:20:45 -0500

    motion!

--- client/src/Pages/MyProfile.js
+++ client/src/Pages/MyProfile.js
@@ -149,6 +149,7 @@ const MyProfile = () => {
   };
 
   const handleReviewSubmit = () => {
+    
     handleCloseReviewModal();
   }
   return (
@@ -183,14 +184,8 @@ const MyProfile = () => {
                     <p>Time: {new Date(item.date_time).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit', hour12: true})}</p>
                     <p>Price: ${item.price}</p>
                   </div>
-<<<<<<< Updated upstream
-                  <button className= "review-button" onClick={() => handleOpenReviewModal(item)}>Write Review</button>
-                </div>
-=======
-                  <button className="review-button" onClick={() => handleOpenReviewModal(item)}>
-  Write Review
-</button>                </div>
->>>>>>> Stashed changes
+                  <button className="review-button" onClick={() => handleOpenReviewModal(item)}>Write Review</button>
+                                  </div>
               ))
             ) : (
               <p>No items ordered</p>
