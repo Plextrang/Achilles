22f3454795d98365f308e599f865d8af2d573264
RaianM <momin.raian@gmail.com>
2024-04-19 15:48:16 -0500

    Last Table?

--- client/src/Pages/SalesReport.js
+++ client/src/Pages/SalesReport.js
@@ -3,6 +3,7 @@ import './SalesReport.css';
 export default function SalesReport(){
     const [salesData, setSalesData] = useState([]);
     const [custData, setCustData] = useState([]);
+    const [dailyData, setDailyData] = useState([]);
 
     useEffect(() => {
       async function fetchSalesData() {
@@ -31,10 +32,33 @@ export default function SalesReport(){
         }
       }
 
+      async function fetchDailyData() {
+        try {
+          const dailyResponse = await fetch('https://cosc-3380-6au9.vercel.app/api/handlers/history/getDailyReport/');
+          if (!dailyResponse.ok) {
+              throw new Error('Failed to fetch customer data');
+          }
+          const dailyData = await dailyResponse.json();
+          setDailyData(dailyData);
+        } catch (error) {
+          console.error('Error fetching customer data:', error);
+        }
+      }
+
       fetchSalesData();
       fetchCustomerData();
+      fetchDailyData();
     }, []);
   
+    function formatTime(dateTime) {
+      const date = new Date(dateTime);
+      const hours = date.getHours();
+      const minutes = date.getMinutes();
+      const ampm = hours >= 12 ? 'PM' : 'AM';
+      const formattedHours = hours % 12 || 12; 
+      const formattedMinutes = minutes < 10 ? '0' + minutes : minutes; 
+      return `${formattedHours}:${formattedMinutes} ${ampm}`;
+    }
   
     return (
       <div className="sales-report-container">
@@ -94,6 +118,34 @@ export default function SalesReport(){
                   <td>{customer.total_transactions ? customer.total_transactions : 0}</td>
                   <td>{customer.units_bought ? customer.units_bought : 0}</td>
                   <td>${customer.total_cost_of_purchases ? customer.total_cost_of_purchases.toFixed(2) : 'N/A'}</td>
+                </tr>
+              ))}
+          </tbody>
+          </table>
+        </div>
+
+        <div className="data-table">
+          <h2>Report 3</h2>
+          <table>
+            <thead>
+              <tr>
+                <th>Column 1</th>
+                <th>Column 2</th>
+                <th>Column 3</th>
+                <th>Column 4</th>
+                <th>Column 5</th>
+              </tr>
+            </thead>
+            <tbody>
+              {dailyData.map(daily => (
+                <tr key={daily.transaction_id}>
+                  <td>{formatTime(daily.date_time)}</td>
+                  <td>{daily.date_time}</td>
+                  <td>{daily.num_of_items}</td>
+                  <td>${daily.total_cost}</td>
+                  <td>{daily.user_id}</td>
+                  <td>{daily.full_name}</td>
+                  <td>{daily.phone_number}</td>
                 </tr>
               ))}
           </tbody>
