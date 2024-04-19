627d7966618734739f0e9b4273adcc83f5be7099
RaianM <momin.raian@gmail.com>
2024-04-19 18:19:34 -0500

    Suppliers have entered the fray!

--- client/src/Pages/AddSupplier.js
+++ client/src/Pages/AddSupplier.js
@@ -3,13 +3,8 @@ import Model from "react-modal";
 import {Link, redirect, useNavigate} from 'react-router-dom'
 import './AddSupplier.css';
 
-
-
-
-
 export default function AddSupplier() {
     const [supplierName, setSupplierName] = useState('');
-    const [supplierID, setsupplierID] = useState('');
     const [supplieremail, setsupplieremail] = useState('');
     const [supplierPhone, setsupplierPhone ] = useState('');
     const [supplierStreet, setsupplierStreet] = useState('');
@@ -20,20 +15,31 @@ export default function AddSupplier() {
     const [visible, setVisible] = useState(true);
 
     const handleSubmit = async(e) =>{
-      const userEmail = localStorage.getItem('userEmail');
       e.preventDefault();
       const supplierData = {
         supplier_name: supplierName,
-        supplier_ID: supplierID,
         supplier_email: supplieremail,
         supplier_phone: supplierPhone,
         supplier_street: supplierStreet,
         supplier_city: supplierCity,
         supplier_state: supplierState,
         supplier_zip: supplierZIP
-      };
-      try{ /*insert backend here i assume */}  
-      catch(error){/*continue backend  here ig  */}
+    };
+    try { 
+        const response = await fetch('https://cosc-3380-6au9.vercel.app/api/handlers/products/newSupplier', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(supplierData)
+        });
+        if (!response.ok) {
+            throw new Error('Failed to add product');
+        }
+    }  
+    catch(error) {
+        console.error('Error adding to Supplier:', error);
+    }
     };
     const close = () => {
         navigate('/Admin');
@@ -54,30 +60,28 @@ export default function AddSupplier() {
                                     <input className="text" type="text" value={supplierName} onChange={(e) => setSupplierName(e.target.value)} placeholder="Enter Supplier's Name" maxLength="50" required />
                                 </div>
                                 <div className="input">
-                                    <label className="text-label">Supplier ID <span className="required">&#42;</span></label>
-                                    <input className="text" type="text" value={supplierID} onChange={(e) => setsupplierID(e.target.value)} placeholder="Enter Supplier ID" maxLength="255" required />
+                                    <label className="text-label">Supplier Email <span className="required">&#42;</span></label>
+                                    <input className="text" type="text" value={supplieremail} onChange={(e) => setsupplieremail(e.target.value)} placeholder="supplier@email.com" maxLength="50" required />
                                 </div>
                                 <div className="input">
-                                    <div className="input">
-                                        <label className="text-label">Supplier Email <span className="required">&#42;</span></label>
-                                        <input className="text" type="text" value={supplieremail} onChange={(e) => setsupplieremail(e.target.value)} placeholder="supplier@email.com" maxLength="50" required />
-                                    </div>
-                                    <div className="input">
-                                        <label className="text-label">Supplier Phone <span className="required">&#42;</span></label>
-                                        <input className="text" type="text" value={supplierPhone} onChange={(e) => setsupplierPhone(e.target.value)} placeholder="123-456-7890" maxLength="10" required />
-                                    </div>
-                                    <div className="input">
-                                        <label className="text-label">Supplier Street <span className="required">&#42;</span></label>
-                                        <input className="text" type="text" value={supplierStreet} onChange={(e) => setsupplierStreet(e.target.value)} placeholder="123 Matcha Ln" maxLength="50" required />
-                                    </div>
-                                    <div className="input">
-                                        <label className="text-label">Supplier City <span className="required">&#42;</span></label>
-                                        <input className="text" type="text" value={supplierCity} onChange={(e) => setsupplierCity(e.target.value)} placeholder="Texas" maxLength="50" required />
-                                    </div>
-                                    <div className="input">
-                                        <label className="text-label">Supplier ZIP <span className="required">&#42;</span></label>
-                                        <input className="text" type="text" value={supplierZIP} onChange={(e) => setsupplierZIP(e.target.value)} placeholder="10101" maxLength="5" required />
-                                    </div>
+                                    <label className="text-label">Supplier Phone <span className="required">&#42;</span></label>
+                                    <input className="text" type="text" value={supplierPhone} onChange={(e) => setsupplierPhone(e.target.value)} placeholder="123-456-7890" maxLength="10" required />
+                                </div>
+                                <div className="input">
+                                    <label className="text-label">Supplier Street <span className="required">&#42;</span></label>
+                                    <input className="text" type="text" value={supplierStreet} onChange={(e) => setsupplierStreet(e.target.value)} placeholder="123 Matcha Ln" maxLength="50" required />
+                                </div>
+                                <div className="input">
+                                    <label className="text-label">Supplier City <span className="required">&#42;</span></label>
+                                    <input className="text" type="text" value={supplierCity} onChange={(e) => setsupplierCity(e.target.value)} placeholder="Houston" maxLength="50" required />
+                                </div>
+                                <div className="input">
+                                    <label className="text-label">Supplier State <span className="required">&#42;</span></label>
+                                    <input className="text" type="text" value={supplierState} onChange={(e) => setsupplierState(e.target.value)} placeholder="Texas" maxLength="50" required />
+                                </div>
+                                <div className="input">
+                                    <label className="text-label">Supplier ZIP <span className="required">&#42;</span></label>
+                                    <input className="text" type="text" value={supplierZIP} onChange={(e) => setsupplierZIP(e.target.value)} placeholder="10101" maxLength="5" required />
                                 </div>
                                 <div className="button-wrapper">
                                     <button className="entry-button" type="submit">Add Supplier</button>
