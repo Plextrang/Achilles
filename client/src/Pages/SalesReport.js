7896d26dc9e9038492c5355071d1c277820b4a5d
RaianM <momin.raian@gmail.com>
2024-04-20 10:14:24 -0500

    Functional filters in report view

--- client/src/Pages/SalesReport.js
+++ client/src/Pages/SalesReport.js
@@ -4,6 +4,7 @@ export default function SalesReport(){
     const [salesData, setSalesData] = useState([]);
     const [custData, setCustData] = useState([]);
     const [dailyData, setDailyData] = useState([]);
+    const [selectedCategory, setSelectedCategory] = useState('Inventory');
 
     useEffect(() => {
       async function fetchSalesData() {
@@ -70,39 +71,45 @@ export default function SalesReport(){
         <div className="report-header-container">
           <h1 className="report-heading">Sales Report</h1>
           <nav className="filters">
-            <a href="#" className="active">Make</a>
-            <a href="#">These</a>
-            <a href="#">Work Later</a>
+          <a href="#" className={selectedCategory === 'Daily' ? 'active' : ''} onClick={() => setSelectedCategory('Daily')}>Daily Sales</a>
+          <a href="#" className={selectedCategory === 'Customer' ? 'active' : ''} onClick={() => setSelectedCategory('Customer')}>Customer</a>
+          <a href="#" className={selectedCategory === 'All' ? 'active' : ''} onClick={() => setSelectedCategory('All')}>All Sales</a>
           </nav>
         </div>
-  
-        <div className="data-table">
-          <h2>Sales Data</h2>
-          <table>
-            <thead>
-              <tr>
-                <th>Shoe Name</th>
-                <th>Shoe ID</th>
-                <th>Units Sold</th>
-                <th>Total Sales</th>
-                <th>Stock Remaining</th>
-                {/* <th>Amount</th> */}
-              </tr>
-            </thead>
-            <tbody>
-              {salesData.map(report => (
-                <tr key={report.product_id}>
-                  <td>{report.item_name}</td>
-                  <td>{report.product_id}</td>
-                  <td>{report.units_sold}</td>
-                  <td>${report.total_sales.toFixed(2)}</td>
-                  <td>{report.stock}</td>
+
+        {selectedCategory === 'Daily' && (
+          <div className="data-table">
+            <h2>Daily Transactions</h2>
+            <table>
+              <thead>
+                <tr>
+                  <th>Transaction ID</th>
+                  <th>Time</th>
+                  <th># of Items</th>
+                  <th>Total Cost</th>
+                  <th>User ID</th>
+                  <th>Customer Name</th>
+                  <th>Phone Number</th>
                 </tr>
-              ))}
+              </thead>
+              <tbody>
+                {dailyData.map(daily => (
+                  <tr key={daily.transaction_id}>
+                    <td>{daily.transaction_id}</td>
+                    <td>{formatTime(daily.date_time)}</td>
+                    <td>{daily.num_of_items}</td>
+                    <td>${daily.total_cost}</td>
+                    <td>{daily.user_id}</td>
+                    <td>{daily.full_name}</td>
+                    <td>{formatPhoneNumber(daily.phone_number)}</td>
+                  </tr>
+                ))}
             </tbody>
-          </table>
-        </div>
-  
+            </table>
+          </div>
+        )}
+
+        {selectedCategory === 'Customer' && (
         <div className="data-table">
           <h2>Customer Details</h2>
           <table>
@@ -128,36 +135,37 @@ export default function SalesReport(){
           </tbody>
           </table>
         </div>
+        )}
 
-        <div className="data-table">
-          <h2>Daily Transactions</h2>
-          <table>
-            <thead>
-              <tr>
-                <th>Transaction ID</th>
-                <th>Time</th>
-                <th># of Items</th>
-                <th>Total Cost</th>
-                <th>User ID</th>
-                <th>Customer Name</th>
-                <th>Phone Number</th>
-              </tr>
-            </thead>
-            <tbody>
-              {dailyData.map(daily => (
-                <tr key={daily.transaction_id}>
-                  <td>{daily.transaction_id}</td>
-                  <td>{formatTime(daily.date_time)}</td>
-                  <td>{daily.num_of_items}</td>
-                  <td>${daily.total_cost}</td>
-                  <td>{daily.user_id}</td>
-                  <td>{daily.full_name}</td>
-                  <td>{formatPhoneNumber(daily.phone_number)}</td>
+        {selectedCategory === 'All' && (
+          <div className="data-table">
+            <h2>Sales Data</h2>
+            <table>
+              <thead>
+                <tr>
+                  <th>Shoe Name</th>
+                  <th>Shoe ID</th>
+                  <th>Units Sold</th>
+                  <th>Total Sales</th>
+                  <th>Stock Remaining</th>
+                  {/* <th>Amount</th> */}
                 </tr>
-              ))}
-          </tbody>
-          </table>
-        </div>
+              </thead>
+              <tbody>
+                {salesData.map(report => (
+                  <tr key={report.product_id}>
+                    <td>{report.item_name}</td>
+                    <td>{report.product_id}</td>
+                    <td>{report.units_sold}</td>
+                    <td>${report.total_sales.toFixed(2)}</td>
+                    <td>{report.stock}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        )}
+
       </div>
     );
     //todo: create a export to pdf button 
